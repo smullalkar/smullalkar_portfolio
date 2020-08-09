30aa03b61829842273b754c2dcdc29fee775d917
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-08-09 19:48:02 +0530

    added more projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,28 +5,58 @@ import styles from './portfolio.module.css'
 
 export default function Projects() {
     return (
-        <div className='container-fluid' style={{backgroundColor:'#FECD55'}}>
-            <br/>
-            <br/>
+        <div className='container-fluid' style={{ backgroundColor: '#FECD55' }}>
+            <br />
+            <br />
             <h1 className={styles.project}>Projects</h1>
             <div className='row p-5'>
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-0 mt-sm-4 mt-4'>
-                    <div className="card" style={{ borderRadius: ' 30px 0 30px 0' }}>
-                        <img src={require('../Components/all-in-one.jpg')} className="card-img-top" alt="img" style={{borderRadius: '30px 0 0 0', height:'250px', width:'100%', backgroundSize:'cover'}}/>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-4 mt-sm-4 mt-4'>
+                    <div className="card" style={{ borderRadius: '0 30px 0 30px' }}>
+                        <img src={require('../Components/marks_recorder.jpg')} className="card-img-top" alt="img" style={{ borderRadius: '0 30px 0 0', height: '250px', width: '100%', backgroundSize: 'cover' }} />
                         <div className="card-body">
-                            <h5 className="card-title" className={styles.projectTitle}>All In One </h5>
-                            <hr/>
-                            <p className="card-text" className={styles.projectDes}>A Fashion store!<br/> this application is made using react, react-router, bootstrap. <br/> Shop for Men, Women and Kids!</p>
+                            <h5 className="card-title" className={styles.projectTitle}>Marks Reccorder </h5>
+                            <hr />
+                            <p className="card-text" className={styles.projectDes}>Student marks managment system!<br /> this application is made using react, react-router, material-ui, flask <br /> Mentain records of student's marks!</p>
                             <div className='row'>
                                 <div className='col-5 d-flex justify-content-end'>
-                                    <a href="https://github.com/masai-course/nj_005_shabaz/tree/master/week_11/day_4/session_1/router-project" ><img alt='img' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
+                                    <a href="https://github.com/smullalkar/Marks_Recorder" ><img alt='img' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
                                 </div>
-                                <div className='col-7 d-flex justify-content-start'>             
-                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position:'relative' ,top:'6px' }} />                       
-                                    <a href='https://router-project.now.sh/' style={{color:'#707070'}}> Demo Link</a>
+                                <div className='col-7 d-flex justify-content-start'>
+                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position: 'relative', top: '6px' }} />
+                                    <a href='marksrecorder.smullalkar.tech' style={{ color: '#707070' }}> Demo Link</a>
                                 </div>
                             </div>
-                            <hr/>
+                            <hr />
+                            <div className='row'>
+                                <div className='col'>
+                                    <img className='mx-2' alt='img' src={require('../Components/react.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/react-router.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/material-ui.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/flask.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/redux.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/aws-ec2.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-4 mt-sm-4 mt-4'>
+                    <div className="card" style={{ borderRadius: ' 30px 0 30px 0' }}>
+                        <img src={require('../Components/all-in-one.jpg')} className="card-img-top" alt="img" style={{ borderRadius: '30px 0 0 0', height: '250px', width: '100%', backgroundSize: 'cover' }} />
+                        <div className="card-body">
+                            <h5 className="card-title" className={styles.projectTitle}>All In One </h5>
+                            <hr />
+                            <p className="card-text" className={styles.projectDes}>A Fashion store!<br /> this application is made using react, react-router, bootstrap. <br /> Shop for Men, Women and Kids!</p>
+                            <div className='row'>
+                                <div className='col-5 d-flex justify-content-end'>
+                                    <a href="https://github.com/smullalkar/Allinone" ><img alt='img' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
+                                </div>
+                                <div className='col-7 d-flex justify-content-start'>
+                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position: 'relative', top: '6px' }} />
+                                    <a href='allinone.smullalkar.tech' style={{ color: '#707070' }}> Demo Link</a>
+                                </div>
+                            </div>
+                            <hr />
                             <div className='row'>
                                 <div className='col'>
                                     <img className='mx-2' alt='img' src={require('../Components/react.svg')} style={{ height: '30px', width: '30px' }}></img>
@@ -37,23 +67,23 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-0 mt-sm-4 mt-4'>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-4 mt-sm-4 mt-4'>
                     <div className="card" style={{ borderRadius: '0 30px 0 30px' }}>
-                        <img src={require('../Components/jobs.jpg')} className="card-img-top" alt="img" style={{borderRadius: '0 30px 0 0', height:'250px', width:'100%', backgroundSize:'cover'}}/>
+                        <img src={require('../Components/jobs.jpg')} className="card-img-top" alt="img" style={{ borderRadius: '0 30px 0 0', height: '250px', width: '100%', backgroundSize: 'cover' }} />
                         <div className="card-body">
                             <h5 className="card-title" className={styles.projectTitle}>Dream Job</h5>
-                            <hr/>
-                            <p className="card-text" className={styles.projectDes}>A Job Portal!<br/> this application is made using react, react-router, bootstrap, axios.<br/> You can search for job here.</p>
+                            <hr />
+                            <p className="card-text" className={styles.projectDes}>A Job Portal!<br /> this application is made using react, react-router, bootstrap, axios.<br /> You can search for job here.</p>
                             <div className='row'>
                                 <div className='col-5 d-flex justify-content-end'>
-                                    <a href="https://github.com/smullalkar/masai-module-3/tree/master/Project/module-3" ><img alt='img' className='' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
+                                    <a href="https://github.com/smullalkar/github-Jobs" ><img alt='img' className='' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
                                 </div>
-                                <div className='col-7 d-flex justify-content-start'>             
-                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position:'relative' ,top:'6px' }} />                       
-                                    <a href='https://github-job-portal.now.sh/' style={{color:'#707070'}}> Demo Link</a>
+                                <div className='col-7 d-flex justify-content-start'>
+                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position: 'relative', top: '6px' }} />
+                                    <a href='githubjobs.smullalkar.tech' style={{ color: '#707070' }}> Demo Link</a>
                                 </div>
                             </div>
-                            <hr/>
+                            <hr />
                             <div className='row'>
                                 <div className='col'>
                                     <img className='mx-2' alt='img' src={require('../Components/react.svg')} style={{ height: '30px', width: '30px' }}></img>
@@ -64,27 +94,27 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-0 mt-sm-4 mt-4'>
-                    <div className="card" style={{borderRadius: '0 30px 0 30px' }}>
-                        <img src={require('../Components/all-in-one.jpg')} className="card-img-top" alt="img" style={{borderRadius: '0 30px 0 0', height:'250px', width:'100%', backgroundSize:'cover'}}/>
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-md-4 mt-lg-4 mt-sm-4 mt-4'>
+                    <div className="card" style={{ borderRadius: ' 30px 0 30px 0' }}>
+                        <img src={require('../Components/covid-19.jpg')} className="card-img-top" alt="img" style={{ borderRadius: '30px 0 0 0', height: '250px', width: '100%', backgroundSize: 'cover' }} />
                         <div className="card-body">
-                            <h5 className="card-title" className={styles.projectTitle}>All In One </h5>
-                            <hr/>
-                            <p className="card-text" className={styles.projectDes}>A Fashion store!<br/> this application is made using react, react-router, bootstrap. <br/> Shop for Men, Women and Kids!</p>
+                            <h5 className="card-title" className={styles.projectTitle}>COVID-19</h5>
+                            <hr />
+                            <p className="card-text" className={styles.projectDes}>COVID stats based on countries!<br /> this application is made using HTML, JQuery, Bootstrap. <br /> See the covid stats of all the countries of the world!</p>
                             <div className='row'>
                                 <div className='col-5 d-flex justify-content-end'>
-                                    <a href="https://github.com/masai-course/nj_005_shabaz/tree/master/week_11/day_4/session_1/router-project" ><img alt='img' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
+                                    <a href="https://github.com/smullalkar/Covid-19" ><img alt='img' src={require('../Components/github-icon.svg')} style={{ height: '30px', width: '30px' }}></img></a>
                                 </div>
-                                <div className='col-7 d-flex justify-content-start'>             
-                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position:'relative' ,top:'6px' }} />                       
-                                    <a href='https://router-project.now.sh/' style={{color:'#707070'}}> Demo Link</a>
+                                <div className='col-7 d-flex justify-content-start'>
+                                    <FontAwesomeIcon icon={faLink} style={{ color: '#707070', marginRight: '5px', position: 'relative', top: '6px' }} />
+                                    <a href='https://covid.smullalkar.tech/' style={{ color: '#707070' }}> Demo Link</a>
                                 </div>
                             </div>
-                            <hr/>
+                            <hr />
                             <div className='row'>
                                 <div className='col'>
-                                    <img className='mx-2' alt='img' src={require('../Components/react.svg')} style={{ height: '30px', width: '30px' }}></img>
-                                    <img className='mx-2' alt='img' src={require('../Components/react-router.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/html-5.svg')} style={{ height: '30px', width: '30px' }}></img>
+                                    <img className='mx-2' alt='img' src={require('../Components/jquery.svg')} style={{ height: '30px', width: '30px' }}></img>
                                     <img className='mx-2' alt='img' src={require('../Components/bootstrap.svg')} style={{ height: '30px', width: '30px' }}></img>
                                 </div>
                             </div>
@@ -92,7 +122,7 @@ export default function Projects() {
                     </div>
                 </div>
             </div>
-            <hr/>
+            <hr />
         </div>
     )
 }
